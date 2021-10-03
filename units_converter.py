3bbd935d97780f6e88383493f2ff172c43484293
staceyso <tokkareva@gmail.com>
2021-10-04 01:24:26 +0300

    Modified units converter

--- units_converter.py
+++ units_converter.py
@@ -3,5 +3,5 @@ import math
 
 def rad_to_degrees(rad):
     degree = rad*180/math.pi
-    print(f'{rad} radians is {degree} degrees')
+    print(f'{rad} radians is {degree} degrees.')
     return degree
