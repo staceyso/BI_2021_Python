ee4607217f61f71f3d43d28654af7dd3dde20053
staceyso <tokkareva@gmail.com>
2021-10-31 17:51:40 +0300

    Fixed the first homework

--- HW1.py
+++ HW1.py
@@ -1,91 +1,74 @@
-# функция "transcribe"
 def transcribe(dna):
-    rna = ''
-    for i in dna:
-        if i == 'T':
-            rna += 'U'
-        elif i == 't':
-            rna += 'u'
-        else:
-            rna += i
-    return rna
+    return dna.replace("T", "U").replace("t", "u")
 
 
-# функция "reverse"
-def reverse(dna):
-    return dna[::-1]
+def reverse(seq):
+    return seq[::-1]
 
 
-# функция "complement"
-def complement(dna):
-    compl_dna = ''
-    compl = dict(A='T', C='G', G='C', T='A', a='t', c='g', g='c', t='a')
-    for i in dna:
-        compl_dna += compl[i]
-    return compl_dna
+def complement(seq):
+    compl_seq = []
+    # list of complementary nucleotides:
+    compl = dict(A='T', C='G', G='C', T='A', U='A', a='t', c='g', g='c', t='a', u='a')
+    for nucl in seq:
+        compl_seq.append(compl[nucl])
+    return "".join(compl_seq)
 
 
-# функция "reverse complement"
-def reverse_complement(dna):
-    compl_dna = ''
-    compl = dict(A='T', C='G', G='C', T='A', a='t', c='g', g='c', t='a')
-    for i in dna:
-        compl_dna += compl[i]
-    return compl_dna[::-1]
+def reverse_complement(seq):
+    compl_seq = []
+    # list of complementary nucleotides:
+    compl = dict(A='T', C='G', G='C', T='A', U='A', a='t', c='g', g='c', t='a', u='a')
+    for nucl in seq:
+        compl_seq.append(compl[nucl])
+    return "".join(compl_seq)[::-1]
 
 
-ok_letters = 'aAcCgGtT'  # буквы, которые встречаются в последовательности ДНК
-while True:  # бесконечный цикл для считывания команд и последовательностей
-    command = input("Enter command: ")  # ввод команды
+def enter_sequence():
+    while True:  # infinite cycle
+        seq = input("Enter your sequence: ")
+        ok_letters = set('aAcCgGtTuU')  # letters that occur in DNA or RNA
+        score = 0  # count for symbols that are not in the "ok_letters"
+        for elem in seq:
+            if elem not in ok_letters:  # checks if the element does not occur in DNA or RNA
+                score += 1  # then score is increased by 1
+        if score != 0:  # if some of the elements do not occur in DNA or RNA
+            print('This DNA or RNA sequence does not exist. Try again')  # repeats the cycle
+        else:  # if everything is ok with the sequence
+            break
+    return seq
+
+
+def enter_dna():
+    while True:  # infinite cycle
+        dna = input("Enter your sequence: ")
+        ok_letters_dna = set('aAcCgGtT')  # letters that occur in DNA
+        score = 0  # count for symbols that are not in the "ok_letters_dna"
+        for elem in dna:
+            if elem not in ok_letters_dna:  # checks if the element does not occur in DNA
+                score += 1  # then score is increased by 1
+        if score != 0:  # if some of the elements do not occur in DNA
+            print('This DNA sequence does not exist. Try again')  # repeats the cycle
+        else:  # if everything is ok with the sequence
+            break
+    return dna
+
+
+while True:  # infinite cycle for entering commands and sequences
+    command = input("Enter command: ")
     if command == 'exit':
-        break  # если команда exit, цикл завершается
-    elif command == 'transcribe':  # команда transcribe
-        while True:
-            seq = input("Enter your sequence: ")  # ввод последовательтности
-            score = 0  # счётчик для проверки правильности последовательности
-            for i in seq:
-                if i not in ok_letters:  # проверка букв в последовательности
-                    score += 1
-            if score != 0:  # счётчик не равен нулю, если в ДНК есть "неправильные буквы"
-                print('This sequence does not exist. Try again')
-            else:  # если с последовательностью всё в порядке
-                break
-        print(transcribe(seq))  # выводится результат функции
-    elif command == 'reverse':  # команда reverse
-        while True:
-            seq = input("Enter your sequence: ")  # ввод последовательтности
-            score = 0  # счётчик для проверки правильности последовательности
-            for i in seq:
-                if i not in ok_letters:  # проверка букв в последовательности
-                    score += 1
-            if score != 0:  # счётчик не равен нулю, если в ДНК есть "неправильные буквы"
-                print('This sequence does not exist. Try again')
-            else:  # если с последовательностью всё в порядке
-                break
-        print(reverse(seq))  # выводится результат функции
-    elif command == 'complement':  # команда complement
-        while True:
-            seq = input("Enter your sequence: ")  # ввод последовательтности
-            score = 0  # счётчик для проверки правильности последовательности
-            for i in seq:
-                if i not in ok_letters:  # проверка букв в последовательности
-                    score += 1
-            if score != 0:  # счётчик не равен нулю, если в ДНК есть "неправильные буквы"
-                print('This sequence does not exist. Try again')
-            else:  # если с последовательностью всё в порядке
-                break
+        break  # if the command is exit, infinite cycle breaks
+    elif command == 'transcribe':
+        dna = enter_dna()  # we use special function to enter DNA sequence
+        print(transcribe(dna))
+    elif command == 'reverse':
+        seq = enter_sequence()  # we use special function to enter DNA or RNA sequence
+        print(reverse(seq))
+    elif command == 'complement':
+        seq = enter_sequence()  # we use special function to enter DNA or RNA sequence
         print(complement(seq))
-    elif command == 'reverse complement':  # команда reverse complement
-        while True:
-            seq = input("Enter your sequence: ")  # ввод последовательтности
-            score = 0  # счётчик для проверки правильности последовательности
-            for i in seq:
-                if i not in ok_letters:  # проверка букв в последовательности
-                    score += 1
-            if score != 0:  # счётчик не равен нулю, если в ДНК есть "неправильные буквы"
-                print('This sequence does not exist. Try again')
-            else:  # если с последовательностью всё в порядке
-                break
+    elif command == 'reverse complement':
+        seq = enter_sequence()  # we use special function to enter DNA or RNA sequence
         print(reverse_complement(seq))  # выводится результат функции
-    else:    # если команда не существует
+    else:    # if the command does not exist in the program
         print('This command does not exist, try again')
