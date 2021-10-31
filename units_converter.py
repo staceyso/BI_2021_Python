1d844ab11f5764a176411910ff8ce97b3c87955e
staceyso <tokkareva@gmail.com>
2021-10-31 16:05:04 +0300

    Modified units converter: interaction with user is added

--- units_converter.py
+++ units_converter.py
@@ -5,3 +5,7 @@ def rad_to_degrees(rad):
     degree = rad*180/math.pi
     print(f'{rad} radians is {degree} degrees.')
     return degree
+
+
+r = int(input('This is radian-degree converter. Enter the angle in radians: '))
+rad_to_degrees(r)
